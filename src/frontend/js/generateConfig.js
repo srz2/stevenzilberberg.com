77496680993e8455ef275a34c345aff6b00974a3
srz2 <srz222@gmail.com>
2024-02-21 14:02:13 -0500

    Moved incorrectly place script tag

--- src/frontend/js/generateConfig.js
+++ src/frontend/js/generateConfig.js
@@ -3,5 +3,5 @@ const {GITHUB_API_KEY} = process.env;
 
 const content = `{\"github_api_key\": \"${GITHUB_API_KEY}\"}`;
 
-fs.writeFileSync('js/config.json', content);
+fs.writeFileSync('config.json', content);
 console.log('Config file generated')
