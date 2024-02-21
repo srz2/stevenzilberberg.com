06d1dd7e0ac3a24049ce201493c934d30245e6ce
srz2 <srz222@gmail.com>
2024-02-20 23:13:54 -0500

    Cleaned up unused code

--- src/frontend/js/github.js
+++ src/frontend/js/github.js
@@ -22,17 +22,6 @@ async function getRepos() {
 async function getProjects(repos) {
     await Promise.all(repos.map(async x => {
         var projectUrl = github_project.replace("{}", x)
-        // reposToDisplay.push({
-        //     "name": "hue_alert",
-        //     "description": "This is a description",
-        //     "url": "https://github.com/srz2/hue_alert",
-        //     "image": "https://images.unsplash.com/photo-1487088678257-3a541e6e3922?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        //     "languages": [
-        //         "Python",
-        //         "HTML",
-        //         "CSS"
-        //     ]
-        // })
         await fetch(projectUrl + github_file_to_search)
         .then(data => data.json())
         .then(async data => {
@@ -78,13 +67,7 @@ async function createCardsFromRepos(){
 }
 
 async function displayProjects() {
-    console.log('Step 1')
     await getRepos();
-    console.log('Step 2')
-    await getProjects(repos);
-    console.log('Step 3')
-    await createCardsFromRepos();
-    console.log('Step 4')
 }
 
 function toTitleCase(str) {
