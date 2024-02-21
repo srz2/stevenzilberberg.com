18f8c2883e25da510848132690fc0bd8959129c7
srz2 <srz222@gmail.com>
2024-02-21 10:53:42 -0500

    Fixed config generation script

--- src/frontend/js/generateConfig.js
+++ src/frontend/js/generateConfig.js
@@ -3,5 +3,5 @@ const {GITHUB_API_KEY} = process.env;
 
 const content = `{\"github_api_key:\": \"${GITHUB_API_KEY}\"}`;
 
-fs.writeFileSync('src/frontend/js/config.json', content);
+fs.writeFileSync('js/config.json', content);
 console.log('Config file generated')
